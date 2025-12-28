c822b9dfa3210ea24c4b9f441c5baafb30bbb5bd
abdelrahman1803 <abdelrahman.cis.eng@gmail.com>
2025-12-29 01:20:27 +0200

    Fix local database pagination slicing

--- lib/features/home/data/data source/home_local_data_source.dart
+++ lib/features/home/data/data source/home_local_data_source.dart
@@ -3,15 +3,24 @@ import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 abstract class HomeLocalDataSource {
-  List<BookEntity> fetchFeaturedBooks();
+  List<BookEntity> fetchFeaturedBooks({int pageNumber = 0});
   List<BookEntity> fetchLatestBooks();
 }
 
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   @override
-  List<BookEntity> fetchFeaturedBooks() {
+  List<BookEntity> fetchFeaturedBooks({int pageNumber = 0}) {
+    int startIndex = pageNumber * 10;
+    int endIndex = (pageNumber + 1) * 10;
     var box = Hive.box<BookEntity>(kFeaturedBox);
-    return box.values.toList();
+    int length = box.length;
+    if (startIndex >= length) {
+      return [];
+    }
+    if (endIndex > length) {
+      endIndex = length;
+    }
+    return box.values.toList().sublist(startIndex, endIndex);
   }
 
   @override
