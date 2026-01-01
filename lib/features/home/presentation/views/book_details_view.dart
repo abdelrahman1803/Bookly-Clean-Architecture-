9c3f59348786c4e7f191140b419783a09d62cbb1
abdelrahman1803 <abdelrahman.cis.eng@gmail.com>
2026-01-01 19:06:23 +0200

    Provide SimilarBooksCubit in BookDetailsView and trigger fetch on init

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,6 +1,11 @@
+import 'package:bookly/core/utilities/services%20locator/dependency_injection.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/use%20cases/fetch_similar_books_use_case.dart';
+import 'package:bookly/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BookDetailsView extends StatelessWidget {
   const BookDetailsView({super.key, required this.book});
@@ -8,8 +13,13 @@ class BookDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: BookDetailsViewBody(book: book)),
+    return BlocProvider(
+      create: (context) =>
+          SimilarBooksCubit(FetchSimilarBooksUseCase(getIt.get<HomeRepoImpl>()))
+            ..fetchSimilarBooks(book.category),
+      child: Scaffold(
+        body: SafeArea(child: BookDetailsViewBody(book: book)),
+      ),
     );
   }
 }
